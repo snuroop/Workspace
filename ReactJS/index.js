3978731f0f2c3f07e96c82af08213428bb8d02eb
snuroop <roopesh.laxman@gmail.com>
2021-12-23 12:44:47 +0530

    updated not found and gitignore

--- ReactJS/index.js
+++ ReactJS/index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 app.get("/", (req, res, next) => {
     res.send("Home page!!");
+});
+app.use((req, res, next) => {
+    res.send("looks like you are lost");
 })
 app.listen(3000, () => {
     console.log("App started!! listening port 3000");
